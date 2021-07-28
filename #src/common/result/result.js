14fb467e44499564de564778b821e6bed51aba3a
Vitalii Radaiev <hf.chelenger@gamil.com>
2021-07-28 15:59:40 +0300

    add page

--- #src/common/result/result.js
+++ #src/common/result/result.js
@@ -1,41 +1,43 @@
 {
 	const slider = document.querySelector('.result__slider');
 	if(slider) {
-		let mySwiper;
+		if(!slider.closest('.result_v2')) {
+			let mySwiper;
 
-		function mobileSlider() {
-			if(document.documentElement.clientWidth < 992 && slider.dataset.mobile == 'false') {
-				mySwiper = new Swiper(slider, {
-					slidesPerView: 1,
-					speed: 600,
-                    loop: true,
-                    // autoplay: {
-                    //     delay: 3000,
-                    //     disableOnInteraction: false,
-                    // },
-                    navigation: {
-                        nextEl: slider.querySelector('.result__btn-next'),
-                        prevEl: slider.querySelector('.result__btn-prev'),
-                    },
-				});
-
-				slider.dataset.mobile = 'true';
-			}
-
-			if(document.documentElement.clientWidth > 991.98) {
-				slider.dataset.mobile = 'false';
-
-				if(slider.classList.contains('swiper-container-initialized')) {
-					mySwiper.destroy();
+			function mobileSlider() {
+				if(document.documentElement.clientWidth < 992 && slider.dataset.mobile == 'false') {
+					mySwiper = new Swiper(slider, {
+						slidesPerView: 1,
+						speed: 600,
+						loop: true,
+						// autoplay: {
+						//     delay: 3000,
+						//     disableOnInteraction: false,
+						// },
+						navigation: {
+							nextEl: slider.querySelector('.result__btn-next'),
+							prevEl: slider.querySelector('.result__btn-prev'),
+						},
+					});
+	
+					slider.dataset.mobile = 'true';
+				}
+	
+				if(document.documentElement.clientWidth > 991.98) {
+					slider.dataset.mobile = 'false';
+	
+					if(slider.classList.contains('swiper-container-initialized')) {
+						mySwiper.destroy();
+					}
 				}
 			}
-		}
-
-		mobileSlider();
-
-		window.addEventListener('resize', () => {
+	
 			mobileSlider();
-		})
+	
+			window.addEventListener('resize', () => {
+				mobileSlider();
+			})
+		}
 	}
 
 }
